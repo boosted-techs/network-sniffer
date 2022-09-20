55430cbdb75146d897eaa2b4e604f2705f2935c0
Ashancy <ashrikan@gmail.com>
2022-09-20 11:26:29 +0300

    new changes

--- sneaky/src/main/main.go
+++ sneaky/src/main/main.go
@@ -26,7 +26,7 @@ import (
 var (
 	device       string = "en0"
 	snapshot_len int32  = 1024
-	promiscuous  bool   = false
+	promiscuous  bool   = true
 	err          error
 	timeout      time.Duration = 30 * time.Second
 	handle       *pcap.Handle
@@ -62,7 +62,7 @@ func main() {
 		var execute bool = false //Evaluates to false if no arguments are supplied
 		/*			Discover new devices on the network */
 		if len(*discoverDevices) > 0 {
-			fmt.Println(SqlAddNIC())
+			SqlAddNIC()
 			execute = true
 		}
 
@@ -121,6 +121,7 @@ func main() {
 			fmt.Println("No Arguments passed")
 		}
 	} else {
+		//findDevices()
 		fmt.Println("Welcome to Sneaky commandline")
 	}
 }
@@ -154,8 +155,9 @@ func SqlAddNIC() string {
 			row["defaultMask"] = ""
 		}
 
-		db.Query("INSERT INTO interfaces (interface, ipv4, ipv6, subnet, description, defaultMask, date_added) value('" + row["deviceName"] + "', '" + row["ip"] + "', '...', '" + row["mask"] + "', '" + row["description"] + "', '" + row["defaultMask"] + "', '" + getDate() + "')")
+		q, _ := db.Query("INSERT INTO interfaces (interface, ipv4, ipv6, subnet, description, defaultMask, date_added) value('" + row["deviceName"] + "', '" + row["ip"] + "', '...', '" + row["mask"] + "', '" + row["description"] + "', '" + row["defaultMask"] + "', '" + getDate() + "')")
 		//fmt.Println(err)
+		defer q.Close()
 
 	}
 	return "Activity ran"
@@ -181,6 +183,7 @@ func findDevices() map[int]map[string]string {
 		for _, address := range device.Addresses {
 
 			_devices[i]["ip"] = address.IP.String()
+			//fmt.Println(address.IP.String())
 			if address.Netmask != nil {
 				_devices[i]["mask"] = address.Netmask.String()
 				a, _ := hex.DecodeString(_devices[i]["mask"])
@@ -207,12 +210,18 @@ func liveCapture() {
 		log.Fatal(err)
 	}
 	defer handle.Close()
-
+	db := dbConnect()
 	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		// Process packet here
-		fmt.Println(packet)
+		//fmt.Println(packet)
+		//fmt.Println("running")
+		_packet := packet.String()
+		d, _err := db.Query("insert into live_packets (device, packet_info, date_added) values('" + device + "', '" + _packet + "', '" + getDate() + "')")
+		d.Close()
+		fmt.Println(_err)
+		fmt.Println(d)
 	}
 }
 
@@ -314,23 +323,33 @@ func localAddresses() {
 	}
 }
 
+func getPacketDetails(content string) string {
+	return content
+}
+
 func printPacketInfo(packet gopacket.Packet) {
 	// Let's see if the packet is an ethernet packet
+	db := dbConnect()
+	//db.SetMaxOpenConns(100000000000000)
 	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
 	if ethernetLayer != nil {
-		fmt.Println("Ethernet layer detected.")
+		//fmt.Println("Ethernet layer detected.")
 		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
-		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
-		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		//fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		//fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
 		// Ethernet type is typically IPv4 but could be ARP or other
-		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
-		fmt.Println()
+		//fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		//fmt.Println()
+		d, _ := db.Query("insert into decoded_packets (message, _from, _to, layer, device, packet_info, date_added, eth_type) values('Ethernet layer detected.', '" + ethernetPacket.SrcMAC.String() + "', '" + ethernetPacket.DstMAC.String() + "', '1', '" + device + "', '', '" + getDate() + "', '" + ethernetPacket.EthernetType.String() + "')")
+		// fmt.Println(_err)
+		// fmt.Println(d)
+		d.Close()
 	}
 
 	// Let's see if the packet is IP (even though the ether type told us)
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer != nil {
-		fmt.Println("IPv4 layer detected.")
+		//fmt.Println("IPv4 layer detected.")
 		ip, _ := ipLayer.(*layers.IPv4)
 
 		// IP layer variables:
@@ -338,43 +357,63 @@ func printPacketInfo(packet gopacket.Packet) {
 		// IHL (IP Header Length in 32-bit words)
 		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
 		// Checksum, SrcIP, DstIP
-		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
-		fmt.Println("Protocol: ", ip.Protocol)
-		fmt.Println()
+		//fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		//fmt.Println("Protocol: ", ip.Protocol)
+		//fmt.Println()
+
+		e, _ := db.Query("insert into decoded_packets (message, _from, _to, layer, device, packet_info, date_added, eth_type, protocal) values('IPv4 layer detected.', '" + ip.SrcIP.String() + "', '" + ip.DstIP.String() + "', '2', '" + device + "', '', '" + getDate() + "', '" + ip.LayerType().String() + "', '" + ip.Protocol.String() + "')")
+		// fmt.Println(g)
+		// fmt.Println(e)
+		e.Close()
+
 	}
 
 	// Let's see if the packet is TCP
 	tcpLayer := packet.Layer(layers.LayerTypeTCP)
 	if tcpLayer != nil {
-		fmt.Println("TCP layer detected.")
+		//fmt.Println("TCP layer detected.")
 		tcp, _ := tcpLayer.(*layers.TCP)
 
 		// TCP layer variables:
 		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
 		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
-		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
-		fmt.Println("Sequence number: ", tcp.Seq)
-		fmt.Println()
+		//fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		//fmt.Println("Sequence number: ", tcp.Seq)
+		//fmt.Println()
+
+		f, _ := db.Query("insert into decoded_packets (message, _from, _to, layer, device, packet_info, date_added, eth_type, sequence_no) values('TCP layer detected.', '" + tcp.SrcPort.String() + "', '" + tcp.SrcPort.String() + "', '3', '" + device + "', '', '" + getDate() + "', '" + tcp.LayerType().String() + "', '" + string(tcp.Seq) + "')")
+		// fmt.Println(g)
+		// fmt.Println(f)
+		f.Close()
 	}
 
 	// Iterate over all layers, printing out each layer type
-	fmt.Println("All packet layers:")
-	for _, layer := range packet.Layers() {
-		fmt.Println("- ", layer.LayerType())
-	}
+	// fmt.Println("All packet layers:")
+	// for _, layer := range packet.Layers() {
+	// 	fmt.Println("- ", layer.LayerType())
+	// }
 
 	// When iterating through packet.Layers() above,
 	// if it lists Payload layer then that is the same as
 	// this applicationLayer. applicationLayer contains the payload
 	applicationLayer := packet.ApplicationLayer()
 	if applicationLayer != nil {
-		fmt.Println("Application layer/Payload found.")
-		fmt.Printf("%s\n", applicationLayer.Payload())
+		// fmt.Println("Application layer/Payload found.")
+		//fmt.Printf("%s\n", applicationLayer.Payload())
 
 		// Search for a string inside the payload
+		var httpMs string = ""
 		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
-			fmt.Println("HTTP found!")
+			httpMs = "HTPP Found"
 		}
+
+		// fmt.Printf("%s\n", applicationLayer.Payload())
+
+		r, _ := db.Query("insert into decoded_packets (message, layer, device, packet_info, date_added, eth_type) values('Application layer/Payload found. " + httpMs + "', '4', '" + device + "', '', '" + getDate() + "', '" + applicationLayer.LayerType().String() + "')")
+		// fmt.Println(r)
+		// fmt.Println(t)
+		r.Close()
+		// fmt.Println()
 	}
 
 	// Check for errors
